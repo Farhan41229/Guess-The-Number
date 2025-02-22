0a0fadc10ddfbe85742a5db3a502f0cbb9a9cff6
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-02-22 08:48:16 +0600

    Principal Work done for the project

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 // Query Selectors
 const message = document.querySelector('.message');
 const number = document.querySelector('.number');
-const socre = document.querySelector('.score');
+const socre_slector = document.querySelector('.score');
 const guess = document.querySelector('.guess');
 const check = document.querySelector('.check');
+const again_selector = document.querySelector('.again');
+const highscore_selector = document.querySelector('.highscore');
+
+// Global Variables
+let score = 20;
+let random = Math.floor(Math.random() * 20 + 1);
+let highscore = 0;
 
 // Game Logic
 check.addEventListener('click', function () {
   const value = Number(guess.value);
   if (!value) {
     message.textContent = `⛔ No number provided`;
+  } else if (value === random) {
+    message.textContent = '🎉 Correct number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+    number.textContent = random;
+    highscore = Math.max(highscore, score);
+    highscore_selector.textContent = highscore;
+  } else if (value < random) {
+    message.textContent = '📉 Too low';
+    score--;
+    socre_slector.textContent = score;
+  } else if (value > random) {
+    message.textContent = '📈 Too high';
+    score--;
+    socre_slector.textContent = score;
   }
 });
 
-
+again_selector.addEventListener('click', function () {
+  score = 20;
+  random = Math.floor(Math.random() * 20 + 1);
+  socre_slector.textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  number.style.width = '15rem';
+  number.textContent = '?';
+  message.textContent = 'Start guessing...';
+  guess.value = '';
+});
